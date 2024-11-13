47ef2b92d54ff28d27b4b888657144c21b8752fb
Nowshin <ibnatnstu@gmail.com>
2024-11-14 00:43:45 +0600

    2nd commit

---
+++ src/Abstraction.java
@@ -0,0 +1,11 @@
+/*l
+* 1. abstract class can have abstract and non abstract method
+* 2. non abstract class can't have abstract method
+* 3. cannot be created object(instantiated) of abstract class
+* 4.
+* */
+public class Abstraction {
+    public static void main(String[] args) {
+
+    }
+}
